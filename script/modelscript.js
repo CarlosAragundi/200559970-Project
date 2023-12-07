e6eed5726dc338fbe2533520483613d13f2da6d2
CarlosAragundi <carlosaragundi13@gmail.com>
2023-12-07 13:22:52 -0500

    Initial commit

---
+++ script/modelscript.js
@@ -0,0 +1,8 @@
+function openSidebar(){
+    document.getElementById("sidebar").style.width = "250px";
+    document.getElementById("main").style.width = "270px";
+}
+function closeSidebar(){
+    document.getElementById("sidebar").style.width = "0";
+    document.getElementById("main").style.width = "0";
+}
